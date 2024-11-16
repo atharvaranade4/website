374cfeb88bb59a6a78ca33113d99f8795d3fe1c0
Atharva Ranade <atharvaranade4@gmail.com>
2024-11-15 17:04:25 -0800

    rename gallery.js

---
+++ client/src/components/gallery/gallery.js
@@ -0,0 +1,46 @@
+'use client'
+import styles from './style.module.css'
+import { useState, useEffect } from 'react'
+import Project from '../project/project'
+import Modal from '../modal/modal'
+import projectData from '../../../public/data/projectsInfo.json'
+
+export default function Home() {
+  const [modal, setModal] = useState({ active: false, index: 0 });
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 1280);
+
+  // Check screen size and update state
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth > 1020);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <div className={styles.gallery}>
+      {projectData.map((project, index) => (
+        <Project 
+          key={index}
+          index={index}
+          title={project.title}
+          projectType={project.projectType}
+          projectDescription={project.description}
+          setModal={setModal}
+          link={project.link}
+          src={project.src} // Add image property for direct display
+          isLargeScreen={isLargeScreen} // Pass screen size condition
+        />
+      ))}
+
+      {/* Show Modal only on large screens */}
+      {isLargeScreen && modal.active && (
+        <Modal
+          modal={modal} 
+          projects={projectData}
+        />
+      )}
+    </div>
+  )
+}
