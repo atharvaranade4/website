76b87bf7eb0eafd7f6baf356a1c0686f3706fd16
Atharva Ranade <atharvaranade4@gmail.com>
2025-01-12 18:07:09 -0800

    Add brand logo img

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,7 +1,8 @@
-// Header.js
 import React from 'react';
+import Image from 'next/image'; // Import Next.js Image component
 import styles from './style.module.scss';
 import { useNavContext } from '../../context/NavContext';
+import logo from '../../../public/images/Logo.svg'; // This path seems correct
 
 function Header() {
   const { toggleVisibility, isVisible, isMobile } = useNavContext();
@@ -9,7 +10,18 @@ function Header() {
   return (
     <header className={styles.appHeader}>
       <div className={styles.content}>
-        <div className={styles.brand}>Logo</div>
+        <div className={styles.brand}>
+          {/* Use Next.js Image component */}
+          <div className={styles.overlayContainer}>
+          <Image
+            src="/images/Logo.svg"
+            alt="Logo"
+            width={isMobile ? 30 : 40}
+            height={isMobile ? 30 : 40}
+            className={styles.Logo}
+            />
+          </div>
+        </div>
         {isMobile && (
           <div className={styles.actions} onClick={toggleVisibility}>
             <div className={`${styles.hamburger} ${isVisible ? styles.open : ''}`}>
