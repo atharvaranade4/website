00dbe691911772b49afea64a45217ddeaeb3e0c5
Atharva Ranade <atharvaranade4@gmail.com>
2025-01-19 15:24:51 -0800

    Update nav component - Setup ScrollTrigger Logic

--- client/src/components/nav/nav.jsx
+++ client/src/components/nav/nav.jsx
@@ -1,12 +1,17 @@
-import React, { useEffect } from 'react';
+import React, {  useEffect, useLayoutEffect, useRef, useState  } from 'react';
+import { usePathname } from 'next/navigation';
 import menuItems from '../../../public/data/menuItems';
 import styles from './styles.module.scss';
 import { useScroll } from '@/context/ScrollContext';
 import { useNavContext } from '../../context/NavContext';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Socials from '../socials/socials';
 
 const Nav = () => {
   const { currentSection, sectionRefs } = useScroll();
   const { isVisible, toggleVisibility, isMobile } = useNavContext();
+  const button = useRef(null);
 
   useEffect(() => {
     // Toggle body scroll based on mobile menu visibility
@@ -16,6 +21,26 @@ const Nav = () => {
       document.body.style.overflow = 'auto';
     };
   }, [isVisible, isMobile]);
+
+
+  // useLayoutEffect(() => {
+  // if (!isMobile) {
+  //   gsap.registerPlugin(ScrollTrigger);
+  //   gsap.to(button.current, {
+  //     scrollTrigger: {
+  //       trigger: document.documentElement,
+  //       start: "top top",
+  //       end: window.innerHeight / 2,
+  //       onLeave: () => {
+  //         gsap.to(button.current, { scale:1, duration: 0.25, ease: "power1.out" });
+  //       },
+  //       onEnterBack: () => {
+  //         gsap.to(button.current, { scale: 0, duration: 0.25, ease: "power1.out" });
+  //       },
+  //     },
+  //   });
+  // }   
+  // }, [isMobile]);
 
   const scrollToSection = (section) => {
     const sectionRef = sectionRefs[section.toLowerCase()];
@@ -40,17 +65,45 @@ const Nav = () => {
       >
         {/* Only render menuItems when visible */}
         {isVisible && (
-          <section className={styles.menuItems}>
-            {menuItems.map((item, index) => (
-              <div
-                key={index}
-                onClick={() => scrollToSection(item)}
-                className={navItemClass(item)}
-              >
-                {item}
-              </div>
-            ))}
-          </section>
+          <div  ref={button} className={styles.navMenu}>
+            <section className={styles.menuItems}>
+              {menuItems.map((item, index) => (
+                <div
+                  key={index}
+                  onClick={() => scrollToSection(item)}
+                  className={navItemClass(item)}
+                >
+                  {item}
+                </div>
+              ))}
+            </section>
+            <span className={styles.socialLinksMenu}>
+              <a
+                href="https://www.instagram.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.socialLinks}
+                >
+                Instagram <span className={styles.arrow}>&#8599;</span>
+              </a>
+              <a
+                href="https://www.linkedin.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.socialLinks}
+                >
+                LinkedIn <span className={styles.arrow}>&#8599;</span>
+              </a>
+              <a
+                href="https://www.github.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.socialLinks}
+                >
+                GitHub <span className={styles.arrow}>&#8599;</span>
+              </a>
+            </span>
+          </div>
         )}
       </nav>
       {/* Overlay appears only on mobile when the menu is visible */}
